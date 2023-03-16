9fad61f5932eacc98623a9a8ca6378c7d408b08a
WolfyZA <connorsommervilles@gmail.com>
2023-03-16 12:39:26 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
 *main - main entry point
-*return of main - will be always 0 (sucessful)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
+/**
+* return - always 0 if sucessful
+*/
