8dc0a2b5c7e58802894c133dc1d62215fa71e615
WolfyZA <connorsommervilles@gmail.com>
2023-03-18 02:43:47 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: 'alphabet in lowercase'
+  *Return: always 0
+  */
+int main(void)
+{
+	int a = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++
+			continue;
+		}
+		putchar(a);
+		n++
+	}
+	putchar('\n');
+	return (0);
+}
