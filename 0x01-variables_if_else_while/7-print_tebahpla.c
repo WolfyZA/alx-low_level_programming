cff60fb8b5be3220cd69479b8885d27a1b9a6f51
WolfyZA <connorsommervilles@gmail.com>
2023-03-18 02:57:18 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: 'alphabet in lowercase backwards'
+  *Return: always 0
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+		{
+		putchar(ch);
+		}
+	putchar('\n');
+	return (0);
+}
