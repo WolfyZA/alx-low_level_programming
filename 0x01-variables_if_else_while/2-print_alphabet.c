4156118881379d54930a1f626f09c56018383592
WolfyZA <connorsommervilles@gmail.com>
2023-03-18 02:23:07 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Description: 'alphabet in lowercase'
+*Return: always 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
