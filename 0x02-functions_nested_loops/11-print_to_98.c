2aa4d857bdba45643ee4bd09b225a1411cc42aea
WolfyZA <connorsommervilles@gmail.com>
2023-03-21 13:34:58 +0000

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  *print_to_98 - prints from n to 98 in order
+  *@n: print result
+  *Return : order of result
+  */
+void print_to_98(int n)
+{
+	int a, b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+			{
+				printf("%d, ", a);
+			}
+			else if (a == 98)
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98, b--)
+		{
+			if (b != 98)
+			{
+				printf("%d, ", b);
+			}
+			else if (b == 98)
+			{
+				printf("%d\n", b);
+			}
+		}
+	}
+}
