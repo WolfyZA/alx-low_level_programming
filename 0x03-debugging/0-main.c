a088abf8c8e5b50d4a62838bf50bc4c25dcd1538
WolfyZA <connorsommervilles@gmail.com>
2023-03-21 22:28:02 +0000

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  *positive_or_negative - print if the integer is negative/zero/positive
+  *main - print if the integer is negative/zero/positive
   *
   *Description: tells the user if their number is negative/zero/positive
   *
-  *@i: integer is 0
   *Return: always 0
   */
-void positive_or_negative(int i)
+int main(void)
 {
 	i = 0;
+	positive_or_negative(i);
 	return (0);
 }
