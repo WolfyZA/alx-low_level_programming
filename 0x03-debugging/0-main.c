5c082e484a7cc75c3d91023b823a2c57eb9a7760
WolfyZA <connorsommervilles@gmail.com>
2023-03-21 22:02:38 +0000

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *positive_or_negative - print if the integer is negative/zero/positive
+  *
+  *return: 0
+  */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
