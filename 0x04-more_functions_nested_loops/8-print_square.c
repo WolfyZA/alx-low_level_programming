8ce4e7882310f016b5aa08cf0471a2f71be88bad
WolfyZA <connorsommervilles@gmail.com>
2023-03-23 12:54:10 +0000

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,13 +6,14 @@
   */
 void print_square(int size)
 {
-	if (size <=0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
 		int i, j;
+
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
