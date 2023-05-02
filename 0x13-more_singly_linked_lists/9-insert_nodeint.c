cea111a0e7946e9841bff71ec9e536a567518b63
WolfyZA <connorsommervilles@gmail.com>
2023-05-02 21:23:53 +0000

    fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			nw->next = tmp->next;
 			tmp->next = nw;
-			return (new);
+			return (nw);
 		}
 		else
 			tmp = tmp->next;
